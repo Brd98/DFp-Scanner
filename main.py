bc663bc6b53e49390513196de2d7c66bad7bfd45
Bred <kingpelmen007@gmail.com>
2025-06-30 22:19:27 +0100

    new hcnage

---
+++ main.py
@@ -0,0 +1,18 @@
+import cmd
+
+class MyNewCLI(cmd.Cmd):
+    prompt = "hello!"
+    intro = "Welcome to the DFp tool!"
+    
+    def do_hello(self, line):
+        """Print a greeting."""
+        print("Hello, World!")
+
+    def do_quit(self, line):
+        """Exit the CLI."""
+        return True
+
+
+
+if __name__ == '__main__':
+    MyNewCLI().cmdloop()
